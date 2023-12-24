d28281131bd37c4f034e51a1cb4e5a6d4fb825bc
Azhar <azharibraheem482@gmail.com>
2023-12-24 14:16:14 +0530

    adding the fetching service

--- src/app/photo-display/photo-display.component.ts
+++ src/app/photo-display/photo-display.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { PhotoApiService } from "../services/photo-api.service";
 @Component({
   selector: 'app-photo-display',
   templateUrl: './photo-display.component.html',
   styleUrls: ['./photo-display.component.css']
 })
 export class PhotoDisplayComponent {
-
+  photoUrl: string = '';
+     constructor(private photosService: PhotoApiService) {
+      this.fetchPhoto()
+     }
+     getRandomPhotos() {
+          this.fetchPhoto()
+     }
+     fetchPhoto() {
+       this.photosService.getPhoto().subscribe((response) => {
+         const {regular} = response.urls
+         this.photoUrl = regular;
+       })
+     }
 }
--- src/app/services/photo-api.service.ts
+++ src/app/services/photo-api.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import {map} from "rxjs";
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoApiService {
- private apiEndPoint = ''
+ private apiEndPoint = 'https://api.unsplash.com/photos/random';
   constructor(private http: HttpClient) {
-
+        http.get(this.apiEndPoint, )
   }
 
-
+ getPhoto() {
+   return this.http.get<UnsplashResponse>(this.apiEndPoint, {
+     headers: {
+       Authorization: 'Client-ID zUToMAG94t_Hpkl461eYp1NsUCkT0E5oIgHJ--MP948'
+     }
+   })
+ }
 }
